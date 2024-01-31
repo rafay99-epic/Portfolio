a9864ecf94d23635061c3d92795844b06bfd7cfe
Abdul Rafay <99marafay@gmail.com>
2024-01-31 18:56:13 +0500

    page Route Error

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,51 @@ import 'frontend/screens/splash_page.dart';
 import 'frontend/theme/light.dart';
 import 'frontend/screens/home_page.dart';
 import 'frontend/screens/service_page.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(const MyApp());
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: lightMode,
+//       home: const SplashScreen(),
+//       onGenerateRoute: (RouteSettings settings) {
+//         switch (settings.name) {
+//           case '/':
+//             return MaterialPageRoute(
+//                 builder: (context) => const SplashScreen());
+//           case '/home':
+//             return MaterialPageRoute(builder: (context) => const HomePage());
+//           case '/services':
+//             return MaterialPageRoute(
+//                 builder: (context) => const ServicesPage());
+//           case '/resume':
+//             return MaterialPageRoute(builder: (context) => const ResumePage());
+//           case '/contact':
+//             return MaterialPageRoute(
+//                 builder: (context) => const ContactMePage());
+//           case '/blog':
+//             return MaterialPageRoute(builder: (context) => const BlogPage());
+//           case '/404':
+//             return MaterialPageRoute(
+//                 builder: (context) => const NotFoundPage());
+//           default:
+//             return MaterialPageRoute(
+//                 builder: (context) => const NotFoundPage());
+//         }
+//       },
+//     );
+//   }
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
