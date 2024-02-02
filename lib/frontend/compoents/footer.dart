2f9d7e612618d58993a010fe87f5f08896451d4b
KHILJI <sameermalik0478@gmail.com>
2024-02-02 22:16:20 +0500

    Comment Footer

--- lib/frontend/compoents/footer.dart
+++ lib/frontend/compoents/footer.dart
@@ -9,46 +9,46 @@ class Footer extends StatelessWidget {
       padding: const EdgeInsets.all(20.0),
       color: Theme.of(context).colorScheme.background,
       child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Made with ',
-              style: TextStyle(
-                fontSize: 12.0,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-            ),
-            Icon(
-              Icons.favorite,
-              color: Theme.of(context).colorScheme.inversePrimary,
-            ),
-            Text(
-              ' by ',
-              style: TextStyle(
-                fontSize: 12.0,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-            ),
-            Text(
-              'Abdul Rafay',
-              style: TextStyle(
-                fontSize: 13.0,
-                color: Theme.of(context).colorScheme.primary,
-                fontWeight: FontWeight.bold,
-                decoration: TextDecoration.underline,
-              ),
-            ),
-            Text(
-              ' using Flutter',
-              style: TextStyle(
-                fontSize: 12.0,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-            ),
-          ],
-        ),
-      ),
+          // child: Row(
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   children: [
+          //     Text(
+          //       'Made with ',
+          //       style: TextStyle(
+          //         fontSize: 12.0,
+          //         color: Theme.of(context).colorScheme.primary,
+          //       ),
+          //     ),
+          //     Icon(
+          //       Icons.favorite,
+          //       color: Theme.of(context).colorScheme.inversePrimary,
+          //     ),
+          //     Text(
+          //       ' by ',
+          //       style: TextStyle(
+          //         fontSize: 12.0,
+          //         color: Theme.of(context).colorScheme.primary,
+          //       ),
+          //     ),
+          //     Text(
+          //       'Abdul Rafay',
+          //       style: TextStyle(
+          //         fontSize: 13.0,
+          //         color: Theme.of(context).colorScheme.primary,
+          //         fontWeight: FontWeight.bold,
+          //         decoration: TextDecoration.underline,
+          //       ),
+          //     ),
+          //     Text(
+          //       ' using Flutter',
+          //       style: TextStyle(
+          //         fontSize: 12.0,
+          //         color: Theme.of(context).colorScheme.primary,
+          //       ),
+          //     ),
+          //   ],
+          // ),
+          ),
     );
   }
 }
