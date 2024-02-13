d318d516ba00c5dccbc9f450e5f0646c7a49e5a8
Abdul Rafay <99marafay@gmail.com>
2024-02-13 10:11:34 +0500

    Code Cleaning

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:rafay_portfolio/admin/backend/auth/auth_gate.dart';
 import 'package:url_strategy/url_strategy.dart';
 
-import 'package:rafay_portfolio/admin/frontend/pages/loginPage.dart';
 import 'package:rafay_portfolio/constants/routes/routes.dart';
 import 'package:rafay_portfolio/user/frontend/screens/NotFound_page.dart';
 import 'user/frontend/screens/splash_page.dart';
@@ -38,9 +37,6 @@ class MyApp extends StatelessWidget {
         return [
           MaterialPageRoute(
             builder: (context) {
-              // return initialRoute == '/'
-              //     ? const SplashScreen()
-              //     : const NotFoundPage();
               return initialRoute == '/'
                   ? const SplashScreen()
                   : initialRoute == '/admin'
@@ -51,80 +47,6 @@ class MyApp extends StatelessWidget {
         ];
       },
       routes: appRoutes,
-      // routes: {
-      //   '/home': (context) => const HomePage(),
-      //   '/projects': (context) => const ProjectGalleryPage(),
-      //   '/resume': (context) => const ResumePage(),
-      //   '/contact': (context) => const ContactMePage(),
-      //   '/blog': (context) => const BlogPage(),
-      //   '/aboutme': (context) => const AboutMePage(),
-      //   '/404': (context) => const NotFoundPage(),
-      // },
     );
   }
 }
-
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-// import 'package:rafay_portfolio/admin/frontend/pages/loginPage.dart';
-// import 'package:url_strategy/url_strategy.dart';
-
-// import 'package:rafay_portfolio/constants/routes/routes.dart';
-// import 'package:rafay_portfolio/user/frontend/screens/NotFound_page.dart';
-// import 'user/frontend/screens/splash_page.dart';
-// import 'constants/theme/light.dart';
-
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: const FirebaseOptions(
-//       apiKey: "AIzaSyBStUH7MVMjr9btBvT4eOBanEIL84FhcvQ",
-//       appId: "1:182119041017:web:f1f49656712460a41f9f28",
-//       messagingSenderId: "182119041017",
-//       projectId: "portfolio-f4698",
-//     ),
-//   );
-//   setPathUrlStrategy();
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       theme: lightMode,
-//       home: StreamBuilder(
-//         stream: FirebaseAuth.instance.authStateChanges(),
-//         builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
-//           if (snapshot.connectionState == ConnectionState.waiting) {
-//             return const CircularProgressIndicator(); // Show loading indicator while waiting
-//           } else {
-//             if (snapshot.hasData) {
-//               return const SplashScreen(); // User is logged in, show the home screen
-//             } else {
-//               return const LoginAdmin(); // User is not logged in, show the login screen
-//             }
-//           }
-//         },
-//       ),
-//       onGenerateInitialRoutes: (String initialRoute) {
-//         return [
-//           MaterialPageRoute(
-//             builder: (context) {
-//               return initialRoute == '/'
-//                   ? const SplashScreen()
-//                   : initialRoute == '/admin'
-//                       ? const LoginAdmin()
-//                       : const NotFoundPage();
-//             },
-//           ),
-//         ];
-//       },
-//       routes: appRoutes,
-//     );
-//   }
-// }
