202e807d677958969bdad4fb677e2d217e06058a
rafay99-epic <82662797+rafay99-epic@users.noreply.github.com>
2024-02-23 11:48:37 +0000

    Build: (c2f6ff0) Action Update: Web & Android Application

--- lib/constants/screensSize/screentype.dart
+++
@@ -1,9 +0,0 @@
-class ScreenType {
-  final double width;
-
-  ScreenType(this.width);
-
-  bool get isMobile => width < 600;
-  bool get isTablet => width >= 600 && width < 1200;
-  bool get isDesktop => width >= 1200;
-}
