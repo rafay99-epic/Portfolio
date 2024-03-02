5a89d25165bdbc5924639b0492b052120874d8da
Abdul Rafay <99marafay@gmail.com>
2024-03-03 03:43:01 +0500

    Blog Page: Adding Blogs

--- lib/admin/backend/model/Blog.dart
+++ lib/admin/backend/model/Blog.dart
@@ -1,89 +1,24 @@
-// enum ContentType { text, image, code }
+// ignore_for_file: file_names
 
-// class ContentBlock {
-//   final ContentType type;
-//   final String content;
+class BlogPosModel {
+  String title;
+  String subTitle;
+  String thumbnail;
+  String url;
+  bool isEnabled;
+  List<String> tags;
+  String author;
+  String content;
 
-//   ContentBlock({
-//     required this.type,
-//     required this.content,
-//   });
-// }
-
-// class BlogPost {
-//   final String title;
-//   final String subTitle;
-//   final String thumbnail;
-//   final String url;
-//   final bool isEnabled;
-//   final List<String> tags;
-//   final DateTime date;
-//   final String author;
-//   final DateTime timestamp;
-//   final List<ContentBlock> contentBlocks;
-
-//   BlogPost({
-//     required this.title,
-//     required this.subTitle,
-//     required this.thumbnail,
-//     required this.url,
-//     required this.isEnabled,
-//     required this.tags,
-//     required this.date,
-//     required this.author,
-//     required this.timestamp,
-//     required this.contentBlocks,
-//   });
-// }
-enum ContentType { text, image, code }
-
-class ContentBlock {
-  final ContentType type;
-  final String content;
-
-  ContentBlock({
-    required this.type,
-    required this.content,
-  });
-
-  Map<String, dynamic> toMap() {
-    return {
-      'type': type.index,
-      'content': content,
-    };
-  }
-
-  static ContentBlock fromMap(Map<String, dynamic> map) {
-    return ContentBlock(
-      type: ContentType.values[map['type']],
-      content: map['content'],
-    );
-  }
-}
-
-class BlogPost {
-  final String title;
-  final String subTitle;
-  final String thumbnail;
-  final String url;
-  final bool isEnabled;
-  final List<String> tags;
-  final DateTime date;
-  final String author;
-  final DateTime timestamp;
-  final List<ContentBlock> contentBlocks;
-
-  BlogPost({
+  BlogPosModel({
     required this.title,
     required this.subTitle,
     required this.thumbnail,
     required this.url,
     required this.isEnabled,
     required this.tags,
-    required this.date,
     required this.author,
-    required this.timestamp,
-    required this.contentBlocks,
+    required this.content,
   });
 
   Map<String, dynamic> toMap() {
@@ -94,26 +29,21 @@ class BlogPost {
       'url': url,
       'isEnabled': isEnabled,
       'tags': tags,
-      'date': date.toIso8601String(),
       'author': author,
-      'timestamp': timestamp.toIso8601String(),
-      'contentBlocks': contentBlocks.map((block) => block.toMap()).toList(),
+      'content': content,
     };
   }
 
-  static BlogPost fromMap(Map<String, dynamic> map) {
-    return BlogPost(
+  static BlogPosModel fromMap(Map<String, dynamic> map) {
+    return BlogPosModel(
       title: map['title'],
       subTitle: map['subTitle'],
       thumbnail: map['thumbnail'],
       url: map['url'],
       isEnabled: map['isEnabled'],
       tags: List<String>.from(map['tags']),
-      date: DateTime.parse(map['date']),
       author: map['author'],
-      timestamp: DateTime.parse(map['timestamp']),
-      contentBlocks: List<ContentBlock>.from(map['contentBlocks']
-          .map((blockMap) => ContentBlock.fromMap(blockMap))),
+      content: map['content'],
     );
   }
 }
--- lib/admin/frontend/pages/blogAdd.dart
+++ lib/admin/frontend/pages/blogAdd.dart
@@ -1,4 +1,159 @@
+// import 'package:flutter/material.dart';
+// import 'package:html_editor_enhanced/html_editor.dart';
+
+// class AddBlogPost extends StatefulWidget {
+//   const AddBlogPost({Key? key}) : super(key: key);
+
+//   @override
+//   State<AddBlogPost> createState() => _AddBlogPostState();
+// }
+
+// class _AddBlogPostState extends State<AddBlogPost> {
+//   HtmlEditorController controller = HtmlEditorController();
+
+//   final _formKey = GlobalKey<FormState>();
+//   String title = '';
+//   String subTitle = '';
+//   String thumbnail = '';
+//   String url = '';
+//   bool isEnabled = false;
+//   List<String> tags = [];
+//   DateTime date = DateTime.now();
+//   String author = '';
+//   DateTime timestamp = DateTime.now();
+//   List<String> contentBlocks = [];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Add Blog Post'),
+//         backgroundColor: Theme.of(context).colorScheme.background,
+//       ),
+//       body: Form(
+//         key: _formKey,
+//         child: ListView(
+//           padding: const EdgeInsets.all(16.0),
+//           children: <Widget>[
+//             //blog title
+//             TextFormField(
+//               decoration: const InputDecoration(
+//                 labelText: 'Title',
+//                 labelStyle: TextStyle(fontSize: 18),
+//               ),
+//               style: const TextStyle(fontSize: 18),
+//               onChanged: (value) {
+//                 setState(() {
+//                   title = value;
+//                 });
+//               },
+//             ),
+//             const SizedBox(height: 20),
+//             // blog description
+//             TextFormField(
+//               decoration: const InputDecoration(
+//                 labelText: 'Description',
+//                 labelStyle: TextStyle(fontSize: 18),
+//               ),
+//               style: const TextStyle(fontSize: 18),
+//               onChanged: (value) {
+//                 setState(() {
+//                   subTitle = value;
+//                 });
+//               },
+//             ),
+//             const SizedBox(height: 20),
+//             //Upload thumbnail
+//             const Text(
+//               "Update Blog Thumbnail",
+//               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+//             ),
+//             const SizedBox(height: 10),
+//             ElevatedButton(
+//               onPressed: () {
+//                 // Implement your image picking logic here
+//               },
+//               child: const Text('Upload Thumbnail'),
+//             ),
+//             const SizedBox(height: 20),
+//             //draft blog
+//             CheckboxListTile(
+//               title: const Text(
+//                 'Enabled',
+//                 style: TextStyle(fontSize: 18),
+//               ),
+//               value: isEnabled,
+//               onChanged: (value) {
+//                 setState(() {
+//                   isEnabled = value ?? false;
+//                 });
+//               },
+//             ),
+//             const SizedBox(height: 20),
+//             TextFormField(
+//               decoration: const InputDecoration(
+//                 labelText: 'Tags (comma separated)',
+//                 labelStyle: TextStyle(fontSize: 18),
+//               ),
+//               style: const TextStyle(fontSize: 18),
+//               onChanged: (value) {
+//                 setState(() {
+//                   tags = value.split(',');
+//                 });
+//               },
+//             ),
+//             TextFormField(
+//               decoration: const InputDecoration(
+//                 labelText: 'Author',
+//                 labelStyle: TextStyle(fontSize: 18),
+//               ),
+//               style: const TextStyle(fontSize: 18),
+//               onChanged: (value) {
+//                 setState(() {
+//                   author = value;
+//                 });
+//               },
+//             ),
+
+//             const SizedBox(height: 20),
+//             HtmlEditor(
+//               controller: controller, //required
+//               htmlEditorOptions: const HtmlEditorOptions(
+//                 hint: "Write yoour Blog here...",
+//               ),
+//               otherOptions: const OtherOptions(
+//                 height: 800,
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//           foregroundColor: Theme.of(context).colorScheme.background,
+//           onPressed: () {
+//             if (_formKey.currentState!.validate()) {
+//               // Save the blog post
+//             }
+//           },
+//           child: const Icon(
+//             Icons.save,
+//           )),
+//     );
+//   }
+// }
+
+// ignore_for_file: prefer_const_constructors, use_build_context_synchronously
+
+import 'dart:io' as io;
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:html_editor_enhanced/html_editor.dart';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
 
 class AddBlogPost extends StatefulWidget {
   const AddBlogPost({Key? key}) : super(key: key);
@@ -8,24 +163,104 @@ class AddBlogPost extends StatefulWidget {
 }
 
 class _AddBlogPostState extends State<AddBlogPost> {
+  HtmlEditorController controller = HtmlEditorController();
+
   final _formKey = GlobalKey<FormState>();
-  String title = '';
-  String subTitle = '';
-  String thumbnail = '';
-  String url = '';
+  final titleController = TextEditingController();
+  final subTitleController = TextEditingController();
+  final tagsController = TextEditingController();
+  final authorController = TextEditingController();
   bool isEnabled = false;
-  List<String> tags = [];
-  DateTime date = DateTime.now();
-  String author = '';
-  DateTime timestamp = DateTime.now();
-  List<String> contentBlocks = [];
+  io.File? _image;
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    subTitleController.dispose();
+    tagsController.dispose();
+    authorController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add Blog Post'),
+        title: const Text('Add Blog Post'),
         backgroundColor: Theme.of(context).colorScheme.background,
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.save),
+            onPressed: () async {
+              if (_formKey.currentState!.validate()) {
+                try {
+                  // Save the blog post
+                  var blogPost = {
+                    'title': titleController.text,
+                    'subTitle': subTitleController.text,
+                    'isEnabled': isEnabled,
+                    'tags': tagsController.text.split(','),
+                    'author': authorController.text,
+                    'content': await controller.getText(),
+                  };
+
+                  // Upload the thumbnail to Firebase Storage
+                  if (_image != null) {
+                    var snapshot = await FirebaseStorage.instance
+                        .ref('thumbnails/${path.basename(_image!.path)}')
+                        .putFile(_image!);
+                    var downloadUrl = await snapshot.ref.getDownloadURL();
+
+                    // Add the download URL to the blog post
+                    blogPost['thumbnail'] = downloadUrl;
+                  } else {
+                    print('No image selected to upload.');
+                  }
+
+                  // Save the blog post to Firestore
+                  await FirebaseFirestore.instance
+                      .collection('blogPosts')
+                      .add(blogPost);
+
+                  // Show a dialog saying that the blog is added
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Blog Added'),
+                      content:
+                          Text('The blog post has been successfully added.'),
+                      actions: <Widget>[
+                        TextButton(
+                          child: Text('OK'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                } catch (e) {
+                  // Show a dialog with the error message
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: Text('Error'),
+                      content: Text(e.toString()),
+                      actions: <Widget>[
+                        TextButton(
+                          child: Text('OK'),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              }
+            },
+          ),
+        ],
       ),
       body: Form(
         key: _formKey,
@@ -33,37 +268,50 @@ class _AddBlogPostState extends State<AddBlogPost> {
           padding: const EdgeInsets.all(16.0),
           children: <Widget>[
             TextFormField(
-              decoration: const InputDecoration(labelText: 'Title'),
-              onChanged: (value) {
-                setState(() {
-                  title = value;
-                });
-              },
+              controller: titleController,
+              decoration: const InputDecoration(
+                labelText: 'Title',
+                labelStyle: TextStyle(fontSize: 18),
+              ),
+              style: const TextStyle(fontSize: 18),
             ),
+            const SizedBox(height: 20),
             TextFormField(
-              decoration: InputDecoration(labelText: 'Sub Title'),
-              onChanged: (value) {
-                setState(() {
-                  subTitle = value;
-                });
-              },
+              controller: subTitleController,
+              decoration: const InputDecoration(
+                labelText: 'Description',
+                labelStyle: TextStyle(fontSize: 18),
+              ),
+              style: const TextStyle(fontSize: 18),
             ),
+            const SizedBox(height: 20),
+            const Text(
+              "Update Blog Thumbnail",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
             ElevatedButton(
-              onPressed: () {
-                // Implement your image picking logic here
+              onPressed: () async {
+                final picker = ImagePicker();
+                final pickedFile =
+                    await picker.pickImage(source: ImageSource.gallery);
+
+                if (pickedFile != null) {
+                  setState(() {
+                    _image = io.File(pickedFile.path);
+                  });
+                } else {
+                  print('No image selected.');
+                }
               },
-              child: Text('Upload Thumbnail'),
+              child: const Text('Upload Thumbnail'),
             ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'URL'),
-              onChanged: (value) {
-                setState(() {
-                  url = value;
-                });
-              },
-            ),
+            const SizedBox(height: 20),
             CheckboxListTile(
-              title: Text('Enabled'),
+              title: const Text(
+                'Enabled',
+                style: TextStyle(fontSize: 18),
+              ),
               value: isEnabled,
               onChanged: (value) {
                 setState(() {
@@ -71,17 +319,135 @@ class _AddBlogPostState extends State<AddBlogPost> {
                 });
               },
             ),
-            // Add more fields here for subTitle, thumbnail, url, isEnabled, tags, date, author, timestamp, and contentBlocks
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: tagsController,
+              decoration: const InputDecoration(
+                labelText: 'Tags (comma separated)',
+                labelStyle: TextStyle(fontSize: 18),
+              ),
+              style: const TextStyle(fontSize: 18),
+            ),
+            TextFormField(
+              controller: authorController,
+              decoration: const InputDecoration(
+                labelText: 'Author',
+                labelStyle: TextStyle(fontSize: 18),
+              ),
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 20),
+            HtmlEditor(
+              controller: controller, //required
+              htmlEditorOptions: const HtmlEditorOptions(
+                hint: "Write your Blog here...",
+              ),
+              otherOptions: const OtherOptions(
+                height: 800,
+              ),
+            ),
+            SizedBox(height: 50),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        foregroundColor: Theme.of(context).colorScheme.background,
+        onPressed: () async {
           if (_formKey.currentState!.validate()) {
             // Save the blog post
+            // var blogPost = {
+            //   'title': titleController.text,
+            //   'subTitle': subTitleController.text,
+            //   'isEnabled': isEnabled,
+            //   'tags': tagsController.text.split(','),
+            //   'author': authorController.text,
+            //   'content': await controller.getText(),
+            // };
+
+            // // Upload the thumbnail to Firebase Storage
+            // if (_image != null) {
+            //   var snapshot = await FirebaseStorage.instance
+            //       .ref('thumbnails/${path.basename(_image!.path)}')
+            //       .putFile(_image!);
+            //   var downloadUrl = await snapshot.ref.getDownloadURL();
+
+            //   // Add the download URL to the blog post
+            //   blogPost['thumbnail'] = downloadUrl;
+            // } else {
+            //   print('No image selected to upload.');
+            // }
+
+            // // Save the blog post to Firestore
+            // await FirebaseFirestore.instance
+            //     .collection('blogPosts')
+            //     .add(blogPost);
+            try {
+              // Save the blog post
+              var blogPost = {
+                'title': titleController.text,
+                'subTitle': subTitleController.text,
+                'isEnabled': isEnabled,
+                'tags': tagsController.text.split(','),
+                'author': authorController.text,
+                'content': await controller.getText(),
+              };
+
+              // Upload the thumbnail to Firebase Storage
+              if (_image != null) {
+                var snapshot = await FirebaseStorage.instance
+                    .ref('thumbnails/${path.basename(_image!.path)}')
+                    .putFile(_image!);
+                var downloadUrl = await snapshot.ref.getDownloadURL();
+
+                // Add the download URL to the blog post
+                blogPost['thumbnail'] = downloadUrl;
+              } else {
+                print('No image selected to upload.');
+              }
+
+              // Save the blog post to Firestore
+              await FirebaseFirestore.instance
+                  .collection('blogPosts')
+                  .add(blogPost);
+
+              // Show a dialog saying that the blog is added
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text('Blog Added'),
+                  content: Text('The blog post has been successfully added.'),
+                  actions: <Widget>[
+                    TextButton(
+                      child: Text('OK'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            } catch (e) {
+              // Show a dialog with the error message
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text('Error'),
+                  content: Text(e.toString()),
+                  actions: <Widget>[
+                    TextButton(
+                      child: Text('OK'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            }
           }
         },
-        child: Icon(Icons.save),
+        child: const Icon(Icons.save),
       ),
     );
   }
