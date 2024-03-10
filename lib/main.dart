3172f84937cb617d66c9626b6c73dfa10a494507
Abdul Rafay <99marafay@gmail.com>
2024-03-10 05:09:36 +0500

    Google Analyziste: Support Add

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,57 @@
+// import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:rafay_portfolio/admin/backend/auth/auth_gate.dart';
+// import 'package:url_strategy/url_strategy.dart';
+
+// import 'package:rafay_portfolio/constants/routes/routes.dart';
+// import 'package:rafay_portfolio/user/frontend/screens/NotFound_page.dart';
+// import 'user/frontend/screens/splash_page.dart';
+// import 'constants/theme/light.dart';
+// import 'package:firebase_analytics/firebase_analytics.dart';
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: const FirebaseOptions(
+//       apiKey: "AIzaSyBStUH7MVMjr9btBvT4eOBanEIL84FhcvQ",
+//       appId: "1:182119041017:web:f1f49656712460a41f9f28",
+//       messagingSenderId: "182119041017",
+//       projectId: "portfolio-f4698",
+//       storageBucket: "portfolio-f4698.appspot.com",
+//       authDomain: "portfolio-f4698.firebaseapp.com",
+//       measurementId: "G-W0TW5XLX4T",
+//     ),
+//   );
+//   setPathUrlStrategy();
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: lightMode,
+//       onGenerateInitialRoutes: (String initialRoute) {
+//         return [
+//           MaterialPageRoute(
+//             builder: (context) {
+//               return initialRoute == '/'
+//                   ? const SplashScreen()
+//                   : initialRoute == '/admin'
+//                       ? const AuthGate()
+//                       : const NotFoundPage();
+//             },
+//           ),
+//         ];
+//       },
+//       routes: appRoutes,
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:rafay_portfolio/admin/backend/auth/auth_gate.dart';
@@ -7,6 +61,7 @@ import 'package:rafay_portfolio/constants/routes/routes.dart';
 import 'package:rafay_portfolio/user/frontend/screens/NotFound_page.dart';
 import 'user/frontend/screens/splash_page.dart';
 import 'constants/theme/light.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,9 +83,14 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  static FirebaseAnalyticsObserver observer =
+      FirebaseAnalyticsObserver(analytics: analytics);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorObservers: <NavigatorObserver>[observer],
       debugShowCheckedModeBanner: false,
       theme: lightMode,
       onGenerateInitialRoutes: (String initialRoute) {
