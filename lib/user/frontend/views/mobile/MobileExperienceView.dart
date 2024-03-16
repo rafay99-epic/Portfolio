9afef0a79538bed82d9fb4b35d51a2c01bead47a
Abdul Rafay <99marafay@gmail.com>
2024-03-16 13:46:43 +0500

    Mobile View

--- lib/user/frontend/views/mobile/MobileExperienceView.dart
+++ lib/user/frontend/views/mobile/MobileExperienceView.dart
@@ -1,1 +1,17 @@
+// ignore_for_file: library_private_types_in_public_api
 
+import 'package:flutter/material.dart';
+
+class MobileExperienceView extends StatefulWidget {
+  const MobileExperienceView({super.key});
+
+  @override
+  _MobileExperienceViewState createState() => _MobileExperienceViewState();
+}
+
+class _MobileExperienceViewState extends State<MobileExperienceView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
