69d599c83689829aaabc45526d5365e6818a06b6
rafay99-epic <82662797+rafay99-epic@users.noreply.github.com>
2024-03-25 14:22:09 +0000

    Build: (0e6b878) Structure Change & Major Code Cleaning

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "232e17f6028a96eb3de55ed3f8b9b305",
-"/": "232e17f6028a96eb3de55ed3f8b9b305",
+const RESOURCES = {"index.html": "387e7b4a75e4ac1d1474bb0aa4eb1a81",
+"/": "387e7b4a75e4ac1d1474bb0aa4eb1a81",
 "_redirects": "d3f02c1e572a02b03f656b8e7b320d3b",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
@@ -70,7 +70,7 @@ const RESOURCES = {"index.html": "232e17f6028a96eb3de55ed3f8b9b305",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "manifest.json": "e06f2e882df95a1f07e00df74d795188",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
-"main.dart.js": "11addf4da4a8c8f244e4fd9e678b39b2"};
+"main.dart.js": "5dfdbd4fbf4f55c722d6689cbad83663"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
